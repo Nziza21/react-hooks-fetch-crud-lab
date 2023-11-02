042493c345b8306c0536ab7a250c265e4ce51b89
Nziza21 <144722415+Nziza21@users.noreply.github.com>
2023-11-03 00:12:26 +0300

    Update QuestionList.js

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem"; 
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => {
+        setQuestions(data);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {questions.map((question) => (
+            <QuestionItem key={question.id} question={question} />
+          ))}
+        </ul>
+      )}
     </section>
   );
 }
